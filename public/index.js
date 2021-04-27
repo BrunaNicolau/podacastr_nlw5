3442340dc3ab2411dd850aa8d1832c2abb843f47
Bruna Nicolau <69939165+BrunaNicolau@users.noreply.github.com>
2021-04-27 20:37:56 +0300

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// oq será apresnetado na pag web, esse root vem de uma tag do index.html 
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+
